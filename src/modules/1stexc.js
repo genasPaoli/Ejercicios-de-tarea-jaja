6d21efbb0805b8fa6ca259d392c490284d5841b3
Genaro Paoli <mgenasp2403@gmail.com>
2025-12-07 01:50:06 -0600

    Primer ejercicio terminado

---
+++ src/modules/1stexc.js
@@ -0,0 +1,31 @@
+/*
+Exercise #1 (sugerencia: join())
+Copy the code below into your workspace:
+*/
+
+let arr = ["This", "is", "a", "sentence."];
+
+// Primero se crea la funcion a partir del array 
+function printOutString() {
+
+// dentro de la funcion se declara una variable que contenga el array con la funcion 
+// .join () que lo que hace es concatenar los strings del array separado por lo que 
+// indique el metodo join dentro de sus comiilas 
+
+  const laOracion = arr.join(" ");
+
+console.log (laOracion)
+
+}
+
+
+printOutString();
+
+
+/*
+¿Qué hace el método join()?
+¿Qué devuelve join() (qué tipo de dato)?
+¿Qué parámetro recibe join()?
+En tu caso específico, ¿qué carácter necesitas entre cada palabra?
+*/
+
