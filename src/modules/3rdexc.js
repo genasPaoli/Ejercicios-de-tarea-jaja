0073c8ee4a54da37bda158826becbdb8b85196a2
Genaro Paoli <mgenasp2403@gmail.com>
2025-12-08 23:56:34 -0600

    Se añade el ejercicio 3

---
+++ src/modules/3rdexc.js
@@ -0,0 +1,10 @@
+/*
+Exercise #3 (sugerencia reduce())
+Write a program to compute the sum and product (multiplication) of an array of 
+numbers. Print out the sum and the product.
+Example: Given an array [1, 2, 3, 4] The sum is 10. The product is 24.
+
+*/
+
+let addnProd = [3, 67, 89 ,23 , 34];
+
