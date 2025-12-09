8a9586f600e78c7f0f4f261e57e633726c4bfa62
Genas <mgenasp2403@gmail.com>
2025-12-09 00:45:26 -0600

    Añado botones en HTML  y un poco de texxto

--- src/script.js
+++ src/script.js
@@ -1,2 +1,14 @@
-import 
+
+muestraelTxt1 () => {
+const texto1 = document.getElementById("1stEx");
+if (texto1.style.display === "none") {
+    texto1.style.display = "block" ;
+}
+    else {
+        texto1.style.display = "none"
+    }
+
+}
+
+
 
